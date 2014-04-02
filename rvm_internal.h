8a75a51639e041735f0d914fdf41608e45fd50f3
Andrew Wilder <andrew.m.wilder@gmail.com>
2014-04-01 23:17:20 -0400

    Initial commit of project files

---
+++ rvm_internal.h
@@ -0,0 +1,15 @@
+/* Andrew Wilder *
+ * Nick Johnson  */
+
+#ifndef __RVM_INTERNAL__
+#define __RVM_INTERNAL__
+
+typedef struct {
+	// TODO
+} rvm_t;
+
+typedef struct {
+	// TODO
+} trans_t;
+
+#endif
